f8ec426f52d2616fb4f3b6fdfa80f91777b9a1fd
Chris Dawes <chris.dawes@mendeley.com>
2016-08-11 14:06:00 +0100

    add missing import

--- src/Manfred/Controllers/KinesisPublisher.cs
+++ src/Manfred/Controllers/KinesisPublisher.cs
@@ -6,6 +6,7 @@ using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using Manfred;
 using Manfred.Models;
+using Amazon.Runtime;
 using Amazon.Kinesis;
 using Amazon.Kinesis.Model;
 using Newtonsoft.Json;
